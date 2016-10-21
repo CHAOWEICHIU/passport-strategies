9dc635f3048c439e8e9881f9d87fce9e6b8ec6e3
Chao Wei Chiu <chaoweichiu@CW-2.local>
2016-10-21 14:33:14 -0400

    added unlink function

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // url : 'mongodb://localhost/auth-passport'
-    url:'mongodb://cw:cw@ds063546.mlab.com:63546/passport'
+    url : 'mongodb://localhost/auth-passport'
+    // url:'mongodb://cw:cw@ds063546.mlab.com:63546/passport'
 }
 
 // var dbUrl = 'mongodb://cw:cw@ds029456.mlab.com:29456/fav-food-recipe'
--- config/passport.js
+++ config/passport.js
@@ -102,49 +102,65 @@ module.exports = (passport)=>{
         clientID        : configAuth.facebookAuth.clientID,
         clientSecret    : configAuth.facebookAuth.clientSecret,
         callbackURL     : configAuth.facebookAuth.callbackURL,
-        profileFields   : configAuth.facebookAuth.scope
+        profileFields   : configAuth.facebookAuth.scope,
+        passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+
     },
 
     // facebook will send back the token and profile
-    function(token, refreshToken, profile, done) {
-        // console.log('token:', token)
-        // console.log('refreshToken', refreshToken)
-        console.log('profile', profile)
-        // asynchronous
-        process.nextTick(function() {
+    function(req, token, refreshToken, profile, done) {
+        if(!req.user){
+            process.nextTick(function() {
+                // find the user in the database based on their facebook id
+                User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
 
-            // find the user in the database based on their facebook id
-            User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+                    // if there is an error, stop everything and return that
+                    // ie an error connecting to the database
+                    if (err) return done(err);
 
-                // if there is an error, stop everything and return that
-                // ie an error connecting to the database
-                if (err) return done(err);
+                    // if the user is found, then log them in
+                    if (user) {
+                        return done(null, user); // user found, return that user
+                    } else {
+                        
+                        // if there is no user found with that facebook id, create them
+                        var newUser            = new User();
 
-                // if the user is found, then log them in
-                if (user) {
-                    return done(null, user); // user found, return that user
-                } else {
-                    
-                    // if there is no user found with that facebook id, create them
-                    var newUser            = new User();
+                        // set all of the facebook information in our user model
+                        newUser.facebook.id    = profile.id; // set the users facebook id                   
+                        newUser.facebook.token = token; // we will save the token that facebook provides to the user                    
+                        newUser.facebook.name  = profile.displayName;
+                        newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
 
-                    // set all of the facebook information in our user model
-                    newUser.facebook.id    = profile.id; // set the users facebook id                   
-                    newUser.facebook.token = token; // we will save the token that facebook provides to the user                    
-                    newUser.facebook.name  = profile.displayName;
-                    newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+                        // save our user to the database
+                        newUser.save(function(err) {
+                            if (err) throw err;
 
-                    // save our user to the database
-                    newUser.save(function(err) {
-                        if (err) throw err;
-
-                        // if successful, return the new user
-                        return done(null, newUser);
-                    });
-                }
-
+                            // if successful, return the new user
+                            return done(null, newUser);
+                        });
+                    }
+                });
             });
-        });
+        } else {
+            var user            = req.user; // pull the user out of the session
+
+            // update the current users facebook credentials
+            user.facebook.id    = profile.id;
+            user.facebook.token = token;
+            user.facebook.name  = profile.displayName;
+            user.facebook.email = profile.emails[0].value;
+
+            // save the user
+            user.save(function(err) {
+                if (err) throw err;
+                return done(null, user);
+            });
+        }
+
+        
+        // asynchronous
+        
 
     }));
 
@@ -157,43 +173,57 @@ module.exports = (passport)=>{
         clientID        : configAuth.googleAuth.clientID,
         clientSecret    : configAuth.googleAuth.clientSecret,
         callbackURL     : configAuth.googleAuth.callbackURL,
-        scope           : configAuth.googleAuth.scope
+        scope           : configAuth.googleAuth.scope,
+        passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
 
     },
-    function(token, refreshToken, profile, done) {
+    function(req, token, refreshToken, profile, done) {
+        if(!req.user){
+            process.nextTick(function() {
+                // try to find the user based on their google id
+                User.findOne({ 'google.id' : profile.id }, function(err, user) {
+                    if (err) return done(err);
 
-        // make the code asynchronous
-        // User.findOne won't fire until we have all our data back from Google
-        process.nextTick(function() {
+                    if (user) {
 
-            // try to find the user based on their google id
-            User.findOne({ 'google.id' : profile.id }, function(err, user) {
-                if (err)
-                    return done(err);
+                        // if a user is found, log them in
+                        return done(null, user);
+                    } else {
+                        // if the user isnt in our database, create a new user
+                        var newUser          = new User();
 
-                if (user) {
+                        // set all of the relevant information
+                        newUser.google.id    = profile.id;
+                        newUser.google.token = token;
+                        newUser.google.name  = profile.displayName;
+                        newUser.google.email = profile.emails[0].value; // pull the first email
 
-                    // if a user is found, log them in
-                    return done(null, user);
-                } else {
-                    // if the user isnt in our database, create a new user
-                    var newUser          = new User();
-
-                    // set all of the relevant information
-                    newUser.google.id    = profile.id;
-                    newUser.google.token = token;
-                    newUser.google.name  = profile.displayName;
-                    newUser.google.email = profile.emails[0].value; // pull the first email
-
-                    // save the user
-                    newUser.save(function(err) {
-                        if (err)
-                            throw err;
-                        return done(null, newUser);
-                    });
-                }
+                        // save the user
+                        newUser.save(function(err) {
+                            if (err)
+                                throw err;
+                            return done(null, newUser);
+                        });
+                    }
+                });
             });
-        });
+        } else {
+            var newUser          = req.user
+
+            // set all of the relevant information
+            newUser.google.id    = profile.id;
+            newUser.google.token = token;
+            newUser.google.name  = profile.displayName;
+            newUser.google.email = profile.emails[0].value; // pull the first email
+
+            // save the user
+            newUser.save(function(err) {
+                if (err)
+                    throw err;
+                return done(null, newUser);
+            });
+        }
+        
 
     }));
 
